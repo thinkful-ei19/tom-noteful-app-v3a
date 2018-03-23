4729befe5122fb816b8a9ea2d27f872969305ed8
thaborak <thaborak@gmail.com>
2018-03-22 20:42:20 -0400

    mlab config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 exports.PORT = process.env.PORT || 8080;
 
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful-app';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://dev:dev@ds119449.mlab.com:19449/noteful';
 
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-app-test';
