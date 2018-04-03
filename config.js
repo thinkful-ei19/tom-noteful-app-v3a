db0ab6e04dbb7e20ee40076c2526c366d920e5f1
thaborak <thaborak@gmail.com>
2018-04-03 14:36:39 -0400

    .env

--- config.js
+++ config.js
@@ -7,3 +7,6 @@ exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://dev:dev@ds119449.mla
 
 
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-app-test';
+
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
